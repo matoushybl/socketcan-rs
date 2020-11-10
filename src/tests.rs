9206d0df7cbeb4ee5791d1ae8d6ffa366d049c28
Matous Hybl <hyblmatous@gmail.com>
2020-11-10 13:07:52 +0100

    Start working on async version of the library.

--- src/tests.rs
+++ src/tests.rs
@@ -1,33 +1,33 @@
-use crate::CANSocket;
-
-#[test]
-fn test_nonexistant_device() {
-    assert!(CANSocket::open("invalid").is_err());
-}
-
-#[test]
-#[cfg(feature = "vcan_tests")]
-fn vcan0_timeout() {
-    let cs = CANSocket::open("vcan0").unwrap();
-    cs.set_read_timeout(std::time::Duration::from_millis(100))
-        .unwrap();
-    assert!(cs.read_frame().should_retry());
-}
-
-#[test]
-#[cfg(feature = "vcan_tests")]
-fn vcan0_set_error_mask() {
-    let cs = CANSocket::open("vcan0").unwrap();
-    cs.error_filter_drop_all().unwrap();
-    cs.error_filter_accept_all().unwrap();
-}
-
-#[test]
-#[cfg(feature = "vcan_tests")]
-fn vcan0_test_nonblocking() {
-    let cs = CANSocket::open("vcan0").unwrap();
-    cs.set_nonblocking(true);
-
-    // no timeout set, but should return immediately
-    assert!(cs.read_frame().should_retry());
-}
+// use crate::CANSocket;
+//
+// #[test]
+// fn test_nonexistant_device() {
+//     assert!(CANSocket::open("invalid").is_err());
+// }
+//
+// #[test]
+// #[cfg(feature = "vcan_tests")]
+// fn vcan0_timeout() {
+//     let cs = CANSocket::open("vcan0").unwrap();
+//     cs.set_read_timeout(std::time::Duration::from_millis(100))
+//         .unwrap();
+//     assert!(cs.read_frame().should_retry());
+// }
+//
+// #[test]
+// #[cfg(feature = "vcan_tests")]
+// fn vcan0_set_error_mask() {
+//     let cs = CANSocket::open("vcan0").unwrap();
+//     cs.error_filter_drop_all().unwrap();
+//     cs.error_filter_accept_all().unwrap();
+// }
+//
+// #[test]
+// #[cfg(feature = "vcan_tests")]
+// fn vcan0_test_nonblocking() {
+//     let cs = CANSocket::open("vcan0").unwrap();
+//     cs.set_nonblocking(true);
+//
+//     // no timeout set, but should return immediately
+//     assert!(cs.read_frame().should_retry());
+// }
